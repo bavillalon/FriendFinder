0f92da19f684218b514edecb036ca4bb284aaa96
Bryan Villalon <bavillalon@gmail.com>
2019-09-18 10:19:10 -0500

    added content to html files and need to customize. adjusted html routes to go to the actual html files and not display dummy information. also added a correctly formated response to the post api. need to add logic to the post api to calculate best friend match.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ var path=require("path");
 
 module.exports= function (app) {
     app.get('*',function(req,res){
-        var url= req.url==="/survey"?"/../public/survey.html":"/../public/index.html";
-        res.end(path.join(__dirname+url));
+        var url= req.url==="/survey"?"/../public/survey.html":"/../public/home.html";
+        res.sendFile(path.join(__dirname+url));
     })
 }
